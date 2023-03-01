6130b986fd727e00be788cc29707a7799b5f4adb
oneharry <harryofficialchidi@yahoo.com>
2023-03-01 21:36:36 +0000

    6,7

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
-  * binary_tree_preorder - Checks if a node is a root
-  * @node: pointer to the tree
+  * binary_tree_preorder - traverse pre-order
+  * @tree: pointer to the tree
   * @func: pointer to the functions
-  * Return: 1 if true, 0 otherwise
+  * Return: void
   */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
